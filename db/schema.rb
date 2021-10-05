489bcc7b84adc0c145eb9bff2147bbf90dfbc237
slicky1 <okunonke@gmail.com>
2021-10-05 08:45:07 -0400

    team reasource created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_05_090211) do
+ActiveRecord::Schema.define(version: 2021_10_05_124148) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,4 +22,14 @@ ActiveRecord::Schema.define(version: 2021_10_05_090211) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.string "img_source"
+    t.bigint "country_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["country_id"], name: "index_teams_on_country_id"
+  end
+
+  add_foreign_key "teams", "countries"
 end
