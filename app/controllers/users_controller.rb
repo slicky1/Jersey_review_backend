a8ca6d0559d5bd017914c7ee467d836cc6744e49
slicky1 <okunonke@gmail.com>
2021-10-05 09:55:57 -0400

    User controller done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,55 @@
+class UsersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+    rescue_from ActiveRecord::RecordInvalid, with: :render_invalid_input
+    
+    
+        def index
+            users = User.all
+            
+            render json: users
+        end
+    
+        def show
+            user = User.find(params[:id])
+            render json: user 
+        end
+    
+        def create
+            user = User.create!(user_params)
+            render json: user, status: :created
+        
+        end
+    
+        def update
+            user = find_user
+            user.update(user_params)
+           render json: user
+        end
+    
+        def destroy
+            user = find_user
+            user.destroy
+           head :no_content
+        end
+    
+    
+    
+        private
+    
+        def user_params
+            params.require(:user).permit(:user_name, :email, :password_digest)
+        end
+    
+        def find_user
+            User.find(params[:id])
+        end
+    
+        def render_not_found
+            render json: { error: "user not found" }, status: :not_found
+        end
+    
+        def render_invalid_input(invalid)
+            render json: {error: invalid.record.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+    
