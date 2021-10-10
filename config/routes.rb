b68c7ed95a5c2977636c41750d70c7328b1fb56b
slicky1 <okunonke@gmail.com>
2021-10-10 14:13:58 -0400

    team controller update

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   resources :reviews
   resources :users
    resources :jerseys
-  resources :brands
-  resources :teams
-  resources :countries
-  
-  
-  # resources :countries do
-  #   resources :teams
-  # end
+  resources :brands do
+    resources :jerseys
+  end
+  # resources :teams
+  # resources :countries
+  resources :countries do
+    resources :teams
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
